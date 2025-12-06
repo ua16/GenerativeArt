1735dc7cef8b16476e9aef5744fb157e7f3c4eb7
ua16 <ua16@users.noreply.github.com>
2025-12-06 17:49:40 +0530

    Made the attraction and repulsion work

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from re import fullmatch
 import pygame
 import math
 import random
@@ -13,6 +12,7 @@ running = True
 # Simulation parameters
 min_distance = 10 # min distance between attractor and fluid particles
 particle_size = 20 # radius of particle size
+particle_repel_range = 20
 
 def distance_between(x1, y1, x2, y2):
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
@@ -29,6 +29,9 @@ class Attractor:
         self.x += self.dx
         self.y += self.dy
 
+    def getVector(self):
+        return pygame.Vector2(self.x, self.y)
+
     
 # Fluid particles move towards the attractor up until a certain point
 # So move towards attractor until at a minimum distance to it
@@ -40,21 +43,40 @@ class FluidParticle:
         self.x = x
         self.y = y
         self.attractor = attractor
+        self.velocity = pygame.Vector2()
         return
 
     def move(self):
-        if distance_between(self.x, self.y, self.attractor.x, self.attractor.y) > min_distance:
-            # move towards attractor, [TODO]
-            self.x += (self.attractor.x - self.x) / 10
-            self.y += (self.attractor.y - self.y) / 10
-        else:
-            # move with attractor
-            self.x += self.attractor.dx
-            self.y += self.attractor.dy
+        # let's have weighted vectors for each 
+        # one for the others
+        # also have a spring that connects to the avg position
+
+        ax = sum(p.x for p in self.attractor.particles) / len(self.attractor.particles)
+        ay = sum(p.y for p in self.attractor.particles) / len(self.attractor.particles)
+        
+        movement = pygame.Vector2()
+
+        for p in self.attractor.particles:
+            if distance_between(self.x, self.y, p.x, p.y) < particle_repel_range:
+                movement += pygame.Vector2(self.x - p.x, self.y - p.y) / 10
+        attractor_distance = distance_between(self.x, self.y, self.attractor.x, self.attractor.y)
+        if 20 < attractor_distance:
+            movement += (self.attractor.getVector() - pygame.Vector2(self.x, self.y)) / 20
+
+        # repel if it's close
+
+        if movement.magnitude() * 0.125 > 0.15:
+            self.velocity += movement * 0.125
+
+        self.velocity *= 0.75
+
+        self.x += self.velocity.x * 0.5
+        self.y += self.velocity.y * 0.5
+
         return
 
 
-simulation = [Attractor(512, 512, 2, 1)]
+simulation = [Attractor(512, 512, 0.001, 0.2)]
 
 for i in range(15):
     att = simulation[0]
@@ -75,6 +97,8 @@ while running:
     screen.fill("black")
 
     # RENDER YOUR GAME HERE
+
+    pygame.draw.circle(screen, "yellow", (512, 512), 5)
 
     for attractor in simulation:
         attractor.move()
